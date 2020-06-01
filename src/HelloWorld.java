affc11f0625b5d6b1e9e5466efc0f4ba4278e19e
Chris Eckert <chris.eckert08@gmail.com>
2020-06-01 17:50:48 -0500

    syntax lec exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,50 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		
+		int myFavoriteNumber = 13;
+		System.out.println(myFavoriteNumber);
+		
+		String myString = "Wubalubadubdub";
+		System.out.println(myString);
+		
+		double myNumber = 3.14;
+		System.out.println(myNumber);
+		
+//		post-incrementation
+//		int x = 5;
+//		System.out.println(x++);
+//		System.out.println(x);
+
+//		pre-incrementation
+//		int x = 5;
+//		System.out.println(++x);
+//		System.out.println(x);
+		
+//		Rewrite the following expressions using the relevant shorthand assignment operators:
+//		int x = 4;
+//		x = x + 5;
+
+//		x += 5;
+//		System.out.println(x);
+		
+//		int x = 3;
+//		int y = 4;
+//		y = y * x;
+////
+//		x *= y;
+//		System.out.println(x);
+		
+//		int x = 10;
+//		int y = 2;
+//		x = x / y;
+//		y = y - x;
+		
+//		x /= y;
+//		y -= x;
+//		System.out.println(y);
+	
+//	byte x = 127;
+//	System.out.println(++x);
 	}
 }
