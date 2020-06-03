8657faed40f8558c85b5e8c8b8bbea440d90017f
Chris Eckert <chris.eckert08@gmail.com>
2020-06-03 11:16:06 -0500

    control flow - exercises - 4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,20 +2,16 @@ public class ControlFlowExercises {
 	
 	public static void main(String[] args) {
 	
-//	for (int i = 5; i <= 15; i++) {
-//		System.out.println(i);
-//	}
-	
-//	for (int i = 0; i <= 100; i += 2) {
-//		System.out.println(i);
-//	}
-	
-//	for (int i = 100; i >= -10; i -= 5) {
-//		System.out.println(i);
-//	}
-	
-	for (double i = 2; i < 1000000; i *= i) {
-		System.out.printf("%.0f%n", i);
+	for (int i = 1; i <= 100; i++) {
+		if (i % 3 == 0 & i % 5 == 0) {
+			System.out.println("FizzBuzz");
+		} else if (i % 3 == 0) {
+			System.out.println("Fizz");
+		} else if (i % 5 == 0) {
+			System.out.println("Buzz");
+		} else {
+			System.out.println(i);
+		}
 	}
 	}
 }
