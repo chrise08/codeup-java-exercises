48781d998bd846cf255fc707cb86f7897714f74d
Chris Eckert <chris.eckert08@gmail.com>
2020-06-03 12:05:30 -0500

    control flow - exercises - 5

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,17 +1,21 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 	
 	public static void main(String[] args) {
 	
-	for (int i = 1; i <= 100; i++) {
-		if (i % 3 == 0 & i % 5 == 0) {
-			System.out.println("FizzBuzz");
-		} else if (i % 3 == 0) {
-			System.out.println("Fizz");
-		} else if (i % 5 == 0) {
-			System.out.println("Buzz");
-		} else {
-			System.out.println(i);
+	Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("What number would you like to go up to?");
+		int userInput = scanner.nextInt();
+		
+		System.out.printf("Here is your table!%n");
+		System.out.printf("%nnumber | squared | cubed%n------ | ------- | -----%n");
+		
+		for (int i = 1; i <= userInput; i++) {
+			int squared = i * i;
+			int cubed = i * i * i;
+			System.out.printf("%-7d| %-8d| %-5d%n", i, squared, cubed);
 		}
-	}
 	}
 }
