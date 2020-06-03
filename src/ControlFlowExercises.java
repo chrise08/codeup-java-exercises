185bc41aa71d802a3c94b5336fbfa98945bb1457
Chris Eckert <chris.eckert08@gmail.com>
2020-06-03 11:02:40 -0500

    control flow - exercises - 2.3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,11 +2,11 @@ public class ControlFlowExercises {
 	
 	public static void main(String[] args) {
 	
-	int i = 100;
+	double i = 2;
 	
 	do {
-		System.out.println(i);
-		i -= 5;
-	} while (i >= -10);
+		System.out.printf("%.0f%n", i);
+		i = Math.pow(i, 2);
+	} while (i < 1000000);
 	}
 }
