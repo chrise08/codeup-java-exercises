325599d7c5fcfbb3b22af888731f321c5fdd0b10
Chris Eckert <chris.eckert08@gmail.com>
2020-06-08 16:13:01 -0500

    methods lecture exercise - 3 (with recursion and best practice)

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,38 +1,7 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-//		System.out.println(addition(5, 7));
-//		System.out.println(subtraction(9, 8));
-//		System.out.println(multiplication(12, 12));
-//		System.out.println(division(27, 9));
-//		System.out.println(modulus(8, 3));
-		
-//		System.out.print("Enter a number between 1 and 10: ");
-//		int userInput = getInteger(1, 10);
-//		long factorial = getFactorial(userInput);
-//
-//		System.out.print("Display factorial? [Y/N] ");
-//		String userContinue = scanner.next();
-//
-//		if (userContinue.equalsIgnoreCase("y")) {
-//			System.out.printf("%d! = %d%n", userInput, factorial);
-//		} else {
-//			System.out.println("Goodbye.");
-//		}
-		
-		System.out.print("Enter the number of sides the dice have: ");
-		int sides = scanner.nextInt();
-		System.out.print("Roll dice? [Y/N] ");
-		String willContinue = scanner.next();
-		if (willContinue.equalsIgnoreCase("y")) {
-			System.out.printf("You rolled: %d and %d%n", diceRoll(sides), diceRoll(sides));
-		} else {
-			System.out.println("See you next time.");
-		}
-		
-	}
+	static Scanner scanner = new Scanner(System.in);
 	
 	public static int addition(int num1, int num2) {
 		return num1 + num2;
@@ -57,6 +26,8 @@ public class MethodsExercises {
 	public static int getInteger(int min, int max) {
 		Scanner scanner = new Scanner(System.in);
 		
+		System.out.printf("Enter a number between %d and %d: ", min, max);
+		
 		int userInput = scanner.nextInt();
 		
 		if (userInput >= min & userInput <= max) {
@@ -68,16 +39,47 @@ public class MethodsExercises {
 		return getInteger(min, max);
 	}
 	
+	// try to use recursion
 	public static long getFactorial(int num) {
 		
-		long fact = 1;
-		for (int i = 1; i <= num; i++) {
-			fact *= i;
+		if (num <= 1) {
+			return 1;
 		}
-		return fact;
+		return num * getFactorial(num - 1);
 	}
 	
 	public static int diceRoll(int num) {
 		return (int)Math.ceil(Math.random() * num);
+	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+//		System.out.println(addition(5, 7));
+//		System.out.println(subtraction(9, 8));
+//		System.out.println(multiplication(12, 12));
+//		System.out.println(division(27, 9));
+//		System.out.println(modulus(8, 3));
+		
+		// Exercise 3 best way
+		int userNumber;
+		String willContinue;
+		
+		do {
+			userNumber = getInteger(1, 26);
+			System.out.printf("Calculate factorial of %d? [Y/N] %n", userNumber);
+			willContinue = scanner.next();
+		} while (!willContinue.equalsIgnoreCase("y"));
+		
+		System.out.printf("%d! = %d%n", userNumber, getFactorial(userNumber));
+		
+//		System.out.print("Enter the number of sides the dice have: ");
+//		int sides = scanner.nextInt();
+//		System.out.print("Roll dice? [Y/N] ");
+//		String willContinue = scanner.next();
+//		if (willContinue.equalsIgnoreCase("y")) {
+//			System.out.printf("You rolled: %d and %d%n", diceRoll(sides), diceRoll(sides));
+//		} else {
+//			System.out.println("See you next time.");
+//		}
+	
 	}
 }
