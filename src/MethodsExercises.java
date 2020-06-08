ec6b98635deb0a9de65eec4e711ae399257fe910
Chris Eckert <chris.eckert08@gmail.com>
2020-06-08 15:06:51 -0500

    methods lecture exercise - 3

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,28 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+//		System.out.println(addition(5, 7));
+//		System.out.println(subtraction(9, 8));
+//		System.out.println(multiplication(12, 12));
+//		System.out.println(division(27, 9));
+//		System.out.println(modulus(8, 3));
+		
+		System.out.print("Enter a number between 1 and 10: ");
+		int userInput = getInteger(1, 10);
+		long factorial = getFactorial(userInput);
+		
+		System.out.print("Display factorial? [Y/N] ");
+		String userContinue = scanner.next();
+		
+		if (userContinue.equalsIgnoreCase("y")) {
+			System.out.printf("%d! = %d%n", userInput, factorial);
+		} else {
+			System.out.println("Goodbye.");
+		}
+		
+	}
 	
 	public static int addition(int num1, int num2) {
 		return num1 + num2;
@@ -28,7 +50,7 @@ public class MethodsExercises {
 		int userInput = scanner.nextInt();
 		
 		if (userInput >= min & userInput <= max) {
-			System.out.println("Your number is within range. Goodbye.");
+			System.out.println("Your number is within range.");
 			return userInput;
 		} else {
 			System.out.println("Number not within range. Try again.");
@@ -36,15 +58,13 @@ public class MethodsExercises {
 		return getInteger(min, max);
 	}
 	
-	public static void main(String[] args) {
-//		System.out.println(addition(5, 7));
-//		System.out.println(subtraction(9, 8));
-//		System.out.println(multiplication(12, 12));
-//		System.out.println(division(27, 9));
-//		System.out.println(modulus(8, 3));
+	public static long getFactorial(int num) {
 		
-		System.out.print("Enter a number between 1 and 10: ");
-		int userInput = getInteger(1, 10);
-		
+		long fact = 1;
+		for (int i = 1; i <= num; i++) {
+			fact *= i;
+		}
+		return fact;
 	}
+	
 }
