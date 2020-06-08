cd0a44a1d2a4a4b8100589ebca14e7d8be7fea8b
Chris Eckert <chris.eckert08@gmail.com>
2020-06-08 12:25:41 -0500

    methods lecture exercise - 2

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
 	
 	public static int addition(int num1, int num2) {
@@ -20,12 +22,29 @@ public class MethodsExercises {
 		return num1 % num2;
 	}
 	
+	public static int getInteger(int min, int max) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int userInput = scanner.nextInt();
+		
+		if (userInput >= min & userInput <= max) {
+			System.out.println("Your number is within range. Goodbye.");
+			return userInput;
+		} else {
+			System.out.println("Number not within range. Try again.");
+		}
+		return getInteger(min, max);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(addition(5, 7));
-		System.out.println(subtraction(9, 8));
-		System.out.println(multiplication(12, 12));
-		System.out.println(division(27, 9));
-		System.out.println(modulus(8, 3));
+//		System.out.println(addition(5, 7));
+//		System.out.println(subtraction(9, 8));
+//		System.out.println(multiplication(12, 12));
+//		System.out.println(division(27, 9));
+//		System.out.println(modulus(8, 3));
+		
+		System.out.print("Enter a number between 1 and 10: ");
+		int userInput = getInteger(1, 10);
 		
 	}
 }
