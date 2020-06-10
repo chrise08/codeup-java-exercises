38df4a554f7dc5bc23e9ffaf0defa12651efe790
Chris Eckert <chris.eckert08@gmail.com>
2020-06-10 10:04:15 -0500

    OOP lecture exercises - 1b

--- src/Person.java
+++ src/Person.java
@@ -25,8 +25,23 @@ public class Person {
 	
 	public static void main(String[] args) {
 		
-		Person person = new Person("Chris");
+//		Person person1 = new Person("John");
+//		Person person2 = new Person("John");
+//		System.out.println(person1.getName().equals(person2.getName())); // returns true
+//		System.out.println(person1 == person2); // returns false
 		
-		person.sayHello();
+//		Person person1 = new Person("John");
+//		Person person2 = person1;
+//		System.out.println(person1 == person2); // returns true
+		
+		Person person1 = new Person("John");
+		Person person2 = person1;
+		System.out.println(person1);
+		System.out.println(person2);
+		System.out.println(person1.getName()); // returns John
+		System.out.println(person2.getName()); // returns John
+		person2.setName("Jane");
+		System.out.println(person1.getName()); // returns Jane because it has same reference id as person2
+		System.out.println(person2.getName()); // returns Jane
 	}
 }
