290ad88c03f2d461f781f038668eb52559b13456
Chris Eckert <chris.eckert08@gmail.com>
2020-06-18 15:28:01 -0500

    arraylist/hashmap lecture - exercise 3

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,10 +1,21 @@
 package grades;
 
 import java.util.HashMap;
+import util.Input;
 
 public class GradesApplication {
 	
+	public static boolean searchAgain() {
+		
+		Input in = new Input();
+		
+		System.out.println("Would you like to see another student?");
+		return in.yesNo();
+	}
+	
 	public static void main(String[] args) {
+		
+		Input in = new Input();
 		
 		HashMap<String, Student> students = new HashMap<>();
 		
@@ -31,6 +42,36 @@ public class GradesApplication {
 		student4.addGrade(80);
 		student4.addGrade(70);
 		students.put("XXthe_cowardly_dogXX", student4);
+		
+		
+		System.out.println("Welcome!");
+		System.out.println();
+		
+		do {
+			System.out.println("Here are the GitHub usernames of our students:");
+			System.out.println();
+			
+			for (String username : students.keySet()) {
+				System.out.printf("|%s| ", username);
+			}
+			
+			System.out.println();
+			System.out.println();
+			System.out.println("What student would you like to see more information on?");
+			String studentToFind = in.getString();
+			
+			if (!students.containsKey(studentToFind)) {
+				System.out.printf("Sorry, no student found with the Github username of \"%s\".%n", studentToFind);
+				System.out.println();
+			} else {
+				System.out.printf("Name: %s - GitHub Username: %s%n"
+				                  + "Current Average: %.2f%n", students.get(studentToFind).getName(), studentToFind,
+				                  students.get(studentToFind).getGradeAverage());
+				System.out.println();
+			}
+		} while(searchAgain());
+		
+		System.out.println("Goodbye, and have a wonderful day!");
 		
 	}
 }
